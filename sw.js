9771983ff242ceb0f818caf3827226c975708a62
ko-izu <matsuitakeshi33@gmail.com>
2025-06-03 21:45:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-var CACHE_NAME = 'pwa-sample-cache-v2';
+var CACHE_NAME = 'pwa-cache';
 var urlsToCache = [
     '/index.html',
     '/manifest.webmanifest',
     '/sw.js',
-"https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.js",
-"https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.css",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
-"https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
-"https://cdnjs.cloudflare.com/ajax/libs/nosleep/0.12.0/NoSleep.min.js",
-"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+    "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.js",
+    "https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.css",
+    "https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js",
+    "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css",
+    "https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/nosleep/0.12.0/NoSleep.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
 ];
 
 self.addEventListener('install', function(event) {
